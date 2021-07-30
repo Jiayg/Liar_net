990b90229a5fffd49075773e35dc0aac368432f9
Jyg <yuguo@7net.cc>
2021-07-30 18:40:18 +0800

    feat:修改分页返回模型

--- src/Liar.Domain.Shared/Dtos/PageModelDto.cs
+++ src/Liar.Domain.Shared/Dtos/PageModelDto.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text.Json.Serialization;
+using System.Xml.Linq;
+using JetBrains.Annotations;
+using Liar.Domain.Shared.Dtos;
 
 namespace Liar.Domain.Shared
 {
@@ -8,8 +11,33 @@ namespace Liar.Domain.Shared
     /// 通用分页信息类
     /// </summary>
     [Serializable]
-    public class PageModelDto<T>
+    public class PageModelDto<T> : IDto
     {
+        private IReadOnlyList<T> _data = Array.Empty<T>();
+        public PageModelDto()
+        {
+        }
+
+        public PageModelDto(SearchPagedDto search) : this(search, default, default)
+        {
+        }
+        public PageModelDto(SearchPagedDto search, IReadOnlyList<T> data, int count, dynamic xData = null) : this(search.Limit, search.Offset, data, count)
+        {
+            this.XData = xData;
+        }
+        public PageModelDto(int limit, int offset, IReadOnlyList<T> data, int count, dynamic xData = null)
+        {
+            this.Limit = limit;
+            this.Offset = offset;
+            this.Total = count;
+            this.Item = data;
+            this.XData = xData;
+        }
+
+        private int Limit { get; set; }
+        private int Offset { get; set; }
+        private dynamic XData { get; set; }
+
         [JsonPropertyName("total")]
         public int Total { get; set; }
 
@@ -17,7 +45,18 @@ namespace Liar.Domain.Shared
         /// 返回数据
         /// </summary>
         [JsonPropertyName("item")]
-        public List<T> Item { get; set; }
-
+        [NotNull]
+        public IReadOnlyList<T> Item
+        {
+            get => _data;
+            set
+            {
+                // ReSharper disable once ConditionIsAlwaysTrueOrFalse
+                if (value != null)
+                {
+                    _data = value;
+                }
+            }
+        }
     }
 }
