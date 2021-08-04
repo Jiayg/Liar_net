58ceba0edff6b2252a7949568da11f0de8baa518
Jyg <yuguo@7net.cc>
2021-08-04 10:48:32 +0800

    feat:增加缓存时长

--- src/Liar.Domain.Shared/Consts/CachingConsts.cs
+++ src/Liar.Domain.Shared/Consts/CachingConsts.cs
@@ -1,5 +1,8 @@
 ﻿namespace Liar.Domain.Shared.Consts
 {
+    /// <summary>
+    /// 时间单位 s
+    /// </summary>
     public class CachingConsts
     {
         public const string LinkChar = ":";
@@ -8,5 +11,7 @@
         public const int OneMonth = 30 * 24 * 60 * 60;
         public const int OneWeek = 7 * 24 * 60 * 60;
         public const int OneDay = 24 * 60 * 60;
+        public const int OneHour = 60 * 60;
+        public const int OneMinute = 60;
     }
 }
