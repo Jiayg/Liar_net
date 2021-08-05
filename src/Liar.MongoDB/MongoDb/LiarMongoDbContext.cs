879e24cbab00e5f7f6c8b1eb36059df8f67a647c
Jyg <yuguo@7net.cc>
2021-08-05 17:58:02 +0800

    feat:增加Mongo服务

---
+++ src/Liar.MongoDB/MongoDb/LiarMongoDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Volo.Abp.MongoDB;
+
+namespace Liar.MongoDB.MongoDb
+{
+    public class LiarMongoDbContext : AbpMongoDbContext
+    {
+        protected override void CreateModel(IMongoModelBuilder modelBuilder)
+        {
+            base.CreateModel(modelBuilder); 
+        }
+    }
+}
---
+++ src/Liar.MongoDB/MongoDb/LiarMongoDbModule.cs
@@ -0,0 +1,42 @@
+﻿using Liar.Core.Microsoft.Extensions.Configuration;
+using Liar.Domain.Shared.ConfigModels;
+using Liar.MongoDB.MongoDb;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp.Data;
+using Volo.Abp.Modularity;
+using Volo.Abp.MongoDB;
+using Volo.Abp.Uow;
+
+namespace Liar
+{
+    [DependsOn(
+        typeof(LiarDomainModule),
+        typeof(LiarDomainSharedModule),
+        typeof(AbpMongoDbModule)
+        )]
+    public class LiarMongoDbModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            var configuration = context.Services.GetConfiguration();
+
+            var mongoConfig = configuration.GetMongoDbSection().Get<MongoConfig>();
+
+            Configure<AbpDbConnectionOptions>(options =>
+            {
+                options.ConnectionStrings.Default = mongoConfig.ConnectionString;
+            });
+
+            context.Services.AddMongoDbContext<LiarMongoDbContext>(options =>
+            {
+                options.AddDefaultRepositories(includeAllEntities: true);
+            });
+
+            Configure<AbpUnitOfWorkDefaultOptions>(options =>
+            {
+                options.TransactionBehavior = UnitOfWorkTransactionBehavior.Disabled;
+            });
+        }
+    }
+}
