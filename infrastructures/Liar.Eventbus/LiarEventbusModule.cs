fee04ef3f0f8b8746dbef034348bb90c08c0ff3c
Jyg <yuguo@7net.cc>
2021-08-05 18:34:35 +0800

    style:修改结构

---
+++ infrastructures/Liar.Eventbus/LiarEventbusModule.cs
@@ -0,0 +1,18 @@
+﻿using Volo.Abp.EventBus;
+using Volo.Abp.EventBus.Abstractions;
+using Volo.Abp.Modularity;
+
+namespace Liar
+{
+    [DependsOn(
+        typeof(AbpEventBusAbstractionsModule),
+        typeof(AbpEventBusModule))
+        ]
+    public class LiarEventbusModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+
+        }
+    }
+}
